1b25f0bcebe408ffbd09e2673a6fd1a626891cfe
Mike Covington <mfcovington@gmail.com>
2013-08-08 14:15:33 -0700

    cleanup

--- merge-boundaries.pl
+++ merge-boundaries.pl
@@ -12,8 +12,6 @@ use feature 'say';
 use Number::RangeTracker;
 use Data::Printer;
 
-# my $range = RangeTracker->new();
-
 my @boundary_files = @ARGV; # || qw(sample-file/RIL_300.boundaries sample-file/RIL_300.boundaries);
 
 # TODO: Extract chromosome lengths from bam file
@@ -40,14 +38,11 @@ for my $chr (@chromosomes) {
 p @boundary_files;
 for my $file (@boundary_files) {
     open my $boundary_fh, "<", $file;
-    # my $range = RangeTracker->new();
     my %boundaries;
     while (<$boundary_fh>) {
         my ($chr, $start, $end ) = split;
         $boundaries{$chr}{$start} = $end;
     }
-# p %boundaries;
-
 
     for my $chr (@chromosomes) {
         my $end = 0;
@@ -75,10 +70,7 @@ say "BORDER SIZES:";
 for my $chr (@chromosomes) {
     say "# of borders in $chr: " . scalar keys $borders{$chr};
     for my $start ( sort { $a <=> $b } keys %{$borders{$chr}} ) {
-        # p $range_ref;
-        # for my $start (keys sort { $a <=> $b } %$range_ref ) {
             say "\t"  . $borders{$chr}->{$start} - $start + 1;
-        # }
     }
 }
 
@@ -86,29 +78,16 @@ say "BIN SIZES:";
 for my $chr (@chromosomes) {
     say "# of bins in $chr: " . scalar keys $bins{$chr};
     for my $start ( sort { $a <=> $b } keys %{$bins{$chr}} ) {
-        # p $range_ref;
-        # for my $start (keys sort { $a <=> $b } %$range_ref ) {
             say "\t"  . $bins{$chr}->{$start} - $start + 1;
-        # }
     }
 }
 
 
 
-# for my $chr (@chromosomes) {
-
-# }
-
-__END__
-
-# $range->add_range( 1, 10 );
-# $range->add_range( 5, 15 );
-# $range->add_range( 50, 150 );
-
-my %borders = $range->output_ranges;
-
-use Data::Printer;
-p %borders;
 
 
 
+
+
+
+exit;
