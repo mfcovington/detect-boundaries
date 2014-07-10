e999a7a30f1ea3dd44595a68882b9e195d871438
Mike Covington <mfcovington@gmail.com>
2014-07-09 21:17:48 -0700

    Change output format of similiar bin finder

--- find-genotype-y-bins-similar-to-genotype-x-bins.pl
+++ find-genotype-y-bins-similar-to-genotype-x-bins.pl
@@ -14,13 +14,19 @@ my ( $query_genotype, $target_genotype, $buffer, @boundaries_files ) = @ARGV;
 
 for my $query_file (@boundaries_files) {
     open my $query_fh, "<", $query_file;
+    my $query_exists = 0;
     while (<$query_fh>) {
         chomp;
         my ( $chr, $start, $end, $genotype ) = split;
         next unless $genotype eq $query_genotype;
+        $query_exists = 1;
+        say join "\t", $query_file, $chr, $start, $end, $genotype;
         find_similar_bins( $chr, $start, $end, $target_genotype, $buffer,
             $query_file, \@boundaries_files );
+        say "";
     }
+    close $query_fh;
+    say "---" if $query_exists;
 }
 
 sub find_similar_bins {
@@ -38,8 +44,7 @@ sub find_similar_bins {
             my $start_diff = abs( $start - $query_start );
             my $end_diff   = abs( $end - $query_end );
             if ( $start_diff < $buffer && $end_diff < $buffer ) {
-                say join "\t", $query_file, $query_chr, $query_start,
-                    $query_end, $query_genotype, $file, $chr, $start, $end,
+                say join "\t", $file, $chr, $start, $end,
                     $genotype;
             }
         }
