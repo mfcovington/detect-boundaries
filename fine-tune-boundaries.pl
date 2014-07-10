a057c87479e952793fcc46c6b48c85a4ecafa0fb
Mike Covington <mfcovington@gmail.com>
2014-07-09 22:24:23 -0700

    Get genotypes

--- fine-tune-boundaries.pl
+++ fine-tune-boundaries.pl
@@ -26,6 +26,7 @@ my @boundaries_files = @ARGV;
 for my $bounds_file (@boundaries_files) {
     my $sample_id  = get_sample_id($bounds_file);
     my $boundaries = get_boundaries($bounds_file);
+    my $genotypes  = get_genotypes( $genotyped_dir, $sample_id );
 
     # say $sample_id;
     # p $boundaries;
@@ -45,8 +46,24 @@ sub get_boundaries {
         chomp;
         my ( $chr, $start, $end, $geno ) = split;
         push @{ $boundaries{$chr} },
-            { start => $start, end => $end, geno => $geno };
+            { 'start' => $start, 'end' => $end, 'geno' => $geno };
     }
     close $bounds_fh;
     return \%boundaries;
+}
+
+sub get_genotypes {
+    my ( $genotyped_dir, $sample_id ) = @_;
+    my %genotypes;
+    my @genotyped_files = glob "$genotyped_dir/$sample_id.*.genotyped*";
+    for my $geno_file (@genotyped_files) {
+        open my $geno_fh, "<", $geno_file;
+        while ( my $line = <$geno_fh> ) {
+            chomp $line;
+            my ( $chr, $pos ) = split /\t/, $line;
+            $genotypes{$chr}{$pos} = $line;
+        }
+        close $geno_fh;
+    }
+    return \%genotypes;
 }
